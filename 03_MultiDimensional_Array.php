5624ba64bd158e8a86fd217f0c14ce20f8d29e14
KareliaConsolidated <kareliaanubhav@outlook.com>
2021-06-20 21:32:05 +0530

    .\03_MultiDimensional_Array.php

---
+++ 03_MultiDimensional_Array.php
@@ -0,0 +1,22 @@
+<?php 
+	$brothers = array(
+		'Joe' => array(
+			'age' => 34,
+			'job' => 'teacher',
+			'state' => 'PA',),
+		'Phil' => array(
+				'age' => 33,
+				'job' => 'photographer',
+				'state' => 'NY',),
+		'Mike' => array(
+					'age' => 32,
+					'job' => 'logistics',
+					'state' => 'NY',)
+	);
+
+	echo '<pre>';
+	print_r( $brothers );
+	echo '</pre>';
+
+	echo '<p>'.$brothers['Phil']['job'].'</p>';
+?>
