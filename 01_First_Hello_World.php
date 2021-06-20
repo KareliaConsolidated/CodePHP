dd3e7373f599ea30d16f1f452feb239e09339653
KareliaConsolidated <kareliaanubhav@outlook.com>
2021-06-20 21:30:52 +0530

    HelloWorld PHP

---
+++ 01_First_Hello_World.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Hello World Script</title>
+	</head>
+	<body>
+		<?php // DEFINE VARIABLES ?>
+		<p><?php echo "Hello PHP"; ?></p>
+
+		<?php // DEFINE CONSTANT ?>
+		<p><?php define('VERSION', 1.1); 
+				 echo VERSION;?>
+		</p>
+	</body>
+</html>
